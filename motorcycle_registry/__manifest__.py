f84afe2b194cb7e93478e01eda551874d7443b92
Carolinetwio <caroline@twio.tech>
2024-01-17 08:16:36 +0000

    [ADD] New Module

---
+++ motorcycle_registry/__manifest__.py
@@ -0,0 +1,14 @@
+{
+    'name': 'Motorcycle Registry',
+    'summary': """Manage Registration of Motorcycles""",
+    'description': """Motorcycle Registry
+        ==============
+        This module is used to keep track of the Motorcycle Registration and 
+        Ownership of each motorcycle of the brand.
+        """,
+    'author': 'Carolinetwio',
+    'version': '0.0.1',
+    'category': 'Kawiil/Custom Modules',
+    'website': 'https://github.com/Carolinetwio/odoo-training',
+    'application': True,
+}
