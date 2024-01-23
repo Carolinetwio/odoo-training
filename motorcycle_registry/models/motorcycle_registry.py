8d191e4d13330f5b41e57dd4f92d5044d4ebe0ad
Carolinetwio <caroline@twio.tech>
2024-01-23 11:27:47 +0100

    [ADD] Add sql constraints and regex patterns

--- motorcycle_registry/models/motorcycle_registry.py
+++ motorcycle_registry/models/motorcycle_registry.py
@@ -1,8 +1,11 @@
 from odoo import api, fields, models
+from odoo.exceptions import ValidationError
+import re
 
 class MotorcycleRegistry(models.Model):
     _name = "motorcycle.registry"
     _description = "Motorcycle Registry"
+    _sql_constraints = [('vin_unique','UNIQUE(vin)','VIN must be unique.')]
     _rec_name = "registry_number"
 
     certificate_title = fields.Binary(string="Certificate Title")
@@ -21,3 +24,15 @@ class MotorcycleRegistry(models.Model):
                 vals['registry_number'] = self.env['ir.sequence'].next_by_code('registry.number')
         return super().create(vals_list)
     
+    @api.constrains('vin')
+    def _check_vin(self):
+        for registry in self:
+            if not (re.match('^[A-Z]{4}\d{2}[A-Z0-9]{2}\d{5}$',registry.vin)):
+                raise ValidationError('The VIN must show the following pattern: [A-Z]{4}\d{2}[A-Z0-9]{2}\d{5}')
+
+    @api.constrains('license_plate')
+    def _check_license_plate(self):
+        for registry in self:
+            if not (re.match('^[A-Z]{1,4}\d{1,3}[A-Z]{0,2}$',registry.license_plate)):
+                raise ValidationError('The License Plate Number must show the following pattern: [A-Z]{1,4]\d{1,3}[A-Z]{0,2}')
+            
